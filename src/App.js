9aab1916aac2c7a8db01e1b1ec03e4feef782d51
pkoppineedi <pkoppineedi@zetaglobal.com>
2025-06-29 17:43:39 +0530

    Refactor App.js to use functional components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 import { v4 as uuid } from "uuid";
 import "./App.css";
@@ -6,34 +5,32 @@ import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
 import Header from './components/Header';
 
-
-
 function App() {
     const LOCAL_STORAGE_KEY = "contacts";
     const [contacts, setContacts] = useState([]);
 
+    // Handler to add a new contact
     const addContactHandler = (contact) => {
         console.log(contact);
         setContacts([...contacts, { id: uuid(), ...contact }]);
     };
 
+    // Handler to remove a contact by id
     const removeContactHandler = (id) => {
-        const newContactList = contacts.filter((contact) => {
-            return contact.id !== id;
-        });
-
+        const newContactList = contacts.filter((contact) => contact.id !== id);
         setContacts(newContactList);
     };
 
+    // Effect to retrieve contacts from local storage on component mount
     useEffect(() => {
-        const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-        if (retriveContacts) setContacts(retriveContacts);
+        const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if (retrieveContacts) setContacts(retrieveContacts);
     }, []);
 
+    // Effect to update local storage whenever contacts change
     useEffect(() => {
         localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
     }, [contacts]);
-
 
     return (
         <div className="ui container">
